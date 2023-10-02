baa76bd146f085e8349c5786c367aa4810c584d5
badonisiddhant15@gmail.com <siddhantbadoni.ar@gmail.com>
2023-10-02 22:53:36 +0530

    update tabbed component and nav animation

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+const nav = document.querySelector('.nav');
+
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove('hidden');
@@ -27,13 +31,71 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
+btnScrollTo.addEventListener('click', function (e) {
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
 
-btnScrollTo.addEventListener('click',function(e){
-section1.scrollIntoView ({behavior : 'smooth'});
-})
+// page navigation
 
+// document.querySelectorAll('.nav__link').forEach(function(el){
+//   el.addEventListener('click',function(e) {
+//   e.preventDefault();
+//   const id = this.getAttribute('href');
+//   document.querySelector(id).scrollIntoView({behavior:'smooth'})
+//   })
+// })
 
+// Event delegation
 
+// 1. Add event listner to common parent element
+// 2. Determine what element originated the event
 
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  // Matching Strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  if (!clicked) return;
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  //Activate tab
+  clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add(`operations__content--active`);
+});
+
+// Menu fade animation
+
+const handleHover = function(e){
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    console.log(link);
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    // console.log(siblinngs)
+    const logo = link.closest('.nav').querySelector('img');
+    siblings.forEach(el => {
+      // console.log(el);
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+}
+// passing "argument" into handler
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+
+nav.addEventListener('mouseout',handleHover.bind(1));
